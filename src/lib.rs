be9db73cafe2a5e05f64af9af289919432287849
KyleSebion <kyle@kylesebion.com>
2025-01-17 22:50:55 -0600

    added proc_pio.rs

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod include {
+    pub mod hardware {
+        pub mod regs {
+            pub mod proc_pio;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", piolib_rs::include::hardware::regs::proc_pio::PROC_PIO_DBG_PADOE_ACCESS);
 }
